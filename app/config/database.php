93e8db56ececda990696a68bfce58fcc4957caaa
Kostadin Stoilov <kmstoilov@gmail.com>
2013-11-09 17:39:20 +0000

    ADD: configuration for mysql database

--- app/config/database.php
+++ app/config/database.php
@@ -55,9 +55,9 @@ return array(
 		'mysql' => array(
 			'driver'    => 'mysql',
 			'host'      => 'localhost',
-			'database'  => 'database',
-			'username'  => 'root',
-			'password'  => '',
+			'database'  => 'sourcenews',
+			'username'  => 'sourcenews',
+			'password'  => 'cyzyCV6xKRYpWejB',
 			'charset'   => 'utf8',
 			'collation' => 'utf8_unicode_ci',
 			'prefix'    => '',
