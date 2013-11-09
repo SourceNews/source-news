11d5c862068dcc90273e0bd2b0489200c448b4f4
Hendrik Appel <hja1g11@soton.ac.uk>
2013-11-09 21:44:37 +0000

    Add the JS for calculating the paragraph and the start end selected charaters

--- app/views/js-test.php
+++ app/views/js-test.php
@@ -1,9 +1,33 @@
 <!doctype html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
-    <title>Source News</title>
+	<meta charset="UTF-8">
+	<title>Source News</title>
+	<script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.3.2/jquery.min.js"></script>
+	<script type="text/javascript" src="js/script.js"></script>
+	<script type="text/javascript">
+	$(function(){
+		addSelectListener(function(data){
+			data && alert(data.name + " " + data.start + "->" + data.end);
+		});
+	});
+	</script>
 </head>
 <body>
+
+<p name="1">It was a dish that was once as synonymous with cockney London as Chas'n'Dave, Pearly Queens and Bow Bells. But now jellied eels, the gelatinous fare that makes even the most enthusiastic omnivore think twice before tucking in, are becoming popular outside the capital for the first time.</p>
+<p name="2">Over the past two years, sales of jellied eels at Tesco have grown by 35% since the supermarket giant took a gamble and started selling them outside London, including in Scotland and Northern Ireland. The supermarket now sells them in nearly 300 of its stores across the UK – a move that it says would have been unthinkable just five years ago.</p>
+<p name="3">It is a major turnaround for a food whose sales plunged during the first decade of this century. The revival is being attributed to a new, more austere environment. "The popularity of jellied eels has taken off during the downturn because shoppers have been looking for inexpensive, nutritious food," said Gary Hooper, a fish buyer for Tesco. "Sales began rising in London and surrounding areas about two years ago, so we decided to take a chance and sell jellied eels further afield."</p>
+<p name="4">The last time eels underwent such a change in fortunes was during the second world war, when rationing was in place and they were a cheap way of feeding a hungry capital.</p>
+<p name="5">Hooper said that he had been surprised that the food was now proving popular outside its traditional home. "I've been in the fisheries business all my life, and until recently no one north of Watford was really interested in eating them," he said.</p>
+<p name="6">"Jellied eels were always considered a regional dish, much like haggis is to Scotland, mushy peas are to northern England and laver bread is to Wales."</p>
+<p name="7">Tesco's supplier, Bradley's, based in Eltham, south-east London, distributes 15,000 pots of jellied eels a week to UK retailers. In the past 10 years it has seen UK sales double from 70 tonnes a year to 140 tonnes.</p>
+<p name="8">Frank Bradley, owner of the 67-year-old family business, said the jellied eel trade had been at risk of all but disappearing before the supermarkets started taking an interest.</p>
+<p name="9">"Until the early 90s, outside of the few remaining eel, pie and mash houses in London, jellied eels were only really sold on old-fashioned seafood stalls outside pubs," Bradley said. "But with the growth in the 90s of pubs serving food, the stall trade died off, and that might have been the end for the jellied eel trade."</p>
+<p name="10">Served either hot or cold, jellied eels are prepared by boiling chopped eels in water and vinegar and then adding lemon, nutmeg and other spices to make a fish stock. The stewing liquid becomes solid and forms a jelly-like substance when cooled.</p>
+<p name="11">According to experts, the dish tastes best with chilli vinegar.</p>
+<p name="12">Eels became a popular food in London during the 18th century – a time when the Thames was so polluted that few actually came from the river. Instead they were brought in by eel barges from Holland.</p>
+<p name="13">According to reports in 2010, eel populations in the Thames had fallen by 98% in just five years. Scientists claimed that the numbers of eels in the river had dropped from 1,500 in 2005 to just 50 in 2010. Today most eels consumed in the UK are imported once again from the Netherlands or Northern Ireland.</p>
+
 </body>
 </html>
---
+++ public/js/script.js
@@ -0,0 +1,44 @@
+/*
+ * Return the start and end of the current selection, undefines otherwise.
+ */
+function getSelectionCharOffsetsWithin() {
+	var start = 0, end = 0;
+	var sel, range, priorRange;
+	if (typeof window.getSelection != "undefined") {
+		range = window.getSelection().getRangeAt(0);
+		if (range.startContainer !== range.endContainer) {
+			console.log("Not the same");
+			return;
+		}
+		if (range.toString().length === 0) {
+			console.log("Nothing selected");
+			return;
+		}
+		priorRange = range.cloneRange();
+		priorRange.selectNodeContents(range.startContainer);
+		priorRange.setEnd(range.startContainer, range.startOffset);
+		start = priorRange.toString().length;
+		end = start + range.toString().length;
+		return {
+			start: start,
+			end: end
+		};
+	} else if (typeof document.selection != "undefined" &&
+			(sel = document.selection).type != "Control") {
+		alert("Please use Chrome or Firefox");
+		return;
+	}
+}
+
+/*
+ * Run the callback when something has been selected with the data
+ * passed in containing the paragraph id and the start and end character
+ * positions
+ */
+function addSelectListener(callback) {
+	$(document.body).bind('mouseup', function(e){
+		var sel = getSelectionCharOffsetsWithin();
+		sel.name = e.target.getAttribute("name");
+		callback(sel)
+	});
+}
