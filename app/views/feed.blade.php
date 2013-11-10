4ecb84b09805106ace2d8edda4ccf9914bf2160b
= <smoussa@hotmail.co.uk>
2013-11-10 16:06:50 +0000

    added news examples

--- app/views/feed.blade.php
+++ app/views/feed.blade.php
@@ -51,22 +51,23 @@
 
                 <div class="list">
                     <div class="top story">
-                        <h2>Sochi Olympic torch taken on historic spacewalk</h2>
+                        <a class="image" href=""><img src="/img/phil.jpg"></a>
+                        <h2>Aid efforts begin after typhoon Haiyan kills 10,000 in Philippines: live updates</h2>
                     </div>
 
                     <div class="story">
-                        <h2>Sochi Olympic torch taken on historic spacewalk</h2>
-                        <p>Two Russian cosmonauts are taking the torch for the Sochi Winter Olympics on its first space journey ...</p>
+                        <h2>Upbeat US jobs data raises prospect of December tapering</h2>
+                        <p>200,000 fresh jobs were created in October, much higher than the 120,000 predicted, prompting speculation ...</p>
                     </div>
 
                     <div class="story">
-                        <h2>Sochi Olympic torch taken on historic spacewalk</h2>
-                        <p>Two Russian cosmonauts are taking the torch for the Sochi Winter Olympics on its first space journey ...</p>
+                        <h2>Ellie Goulding: 'My voice is the top thing. That's the key'</h2>
+                        <p>With several awards and a No 1 to her name, the singer has come a long way since she was embarrassed by how she spoke.  ...</p>
                     </div>
                     
                     <div class="story">
-                        <h2>Sochi Olympic torch taken on historic spacewalk</h2>
-                        <p>Two Russian cosmonauts are taking the torch for the Sochi Winter Olympics on its first space journey ...</p>
+                        <h2>Tim Krul inspires Newcastle United to three points at Tottenham Hotspur</h2>
+                        <p>The pre-match discussion had been about the absence of one goalkeeper and there ...</p>
                     </div>
 
                     <div class="story">
@@ -87,34 +88,35 @@
 
                 <div class="list">
                     <div class="top story">
-                        <a class="image" href=""><img src=""></a>
-                        <h2>Sochi Olympic torch taken on historic spacewalk</h2>
-                    </div>
-
-                    <div class="story">
-                        <h2>Sochi Olympic torch taken on historic spacewalk</h2>
-                        <p>Two Russian cosmonauts are taking the torch for the Sochi Winter Olympics on its first space journey ...</p>
-                    </div>
-
-                    <div class="story">
-                        <h2>Sochi Olympic torch taken on historic spacewalk</h2>
-                        <p>Two Russian cosmonauts are taking the torch for the Sochi Winter Olympics on its first space journey ...</p>
+                        <a class="image" href=""><img src="/img/em.jpg"></a>
+                        <h2>Miliband calls for payday loan TV ads to children to be banned</h2>
                     </div>
                     
                     <div class="story">
                         <h2>Sochi Olympic torch taken on historic spacewalk</h2>
                         <p>Two Russian cosmonauts are taking the torch for the Sochi Winter Olympics on its first space journey ...</p>
                     </div>
+                    
+                    <div class="story">
+                        <h2>Tim Krul inspires Newcastle United to three points at Tottenham Hotspur</h2>
+                        <p>The pre-match discussion had been about the absence of one goalkeeper and there ...</p>
+                    </div>
+
+                    <div class="story">
+                        <h2>Upbeat US jobs data raises prospect of December tapering</h2>
+                        <p>200,000 fresh jobs were created in October, much higher than the 120,000 predicted, prompting speculation ...</p>
+                    </div>
 
                     <div class="story">
                         <h2>Sochi Olympic torch taken on historic spacewalk</h2>
                         <p>Two Russian cosmonauts are taking the torch for the Sochi Winter Olympics on its first space journey ...</p>
                     </div>
-                    
+
                     <div class="story">
-                        <h2>Sochi Olympic torch taken on historic spacewalk</h2>
-                        <p>Two Russian cosmonauts are taking the torch for the Sochi Winter Olympics on its first space journey ...</p>
+                        <h2>Ellie Goulding: 'My voice is the top thing. That's the key'</h2>
+                        <p>With several awards and a No 1 to her name, the singer has come a long way since she was embarrassed by how she spoke.  ...</p>
                     </div>
+
                 </div>
 
             </div>
@@ -124,13 +126,13 @@
 
                 <div class="list">
                     <div class="top story">
-                        <a class="image" href=""><img src=""></a>
-                        <h2>Sochi Olympic torch taken on historic spacewalk</h2>
+                        <a class="image" href=""><img src="/img/queen.jpg"></a>
+                        <h2>Remembrance Sunday: UK falls silent to commemorate war dead</h2>
                     </div>
 
                     <div class="story">
-                        <h2>Sochi Olympic torch taken on historic spacewalk</h2>
-                        <p>Two Russian cosmonauts are taking the torch for the Sochi Winter Olympics on its first space journey ...</p>
+                        <h2>Iran: Nuclear deal can be done says Hague</h2>
+                        <p>A deal can be reached with Iran to curb its nuclear programme, UK Foreign Secretary ...</p>
                     </div>
 
                     <div class="story">
@@ -140,7 +142,7 @@
                     
                     <div class="story">
                         <h2>Sochi Olympic torch taken on historic spacewalk</h2>
-                        <p>Two Russian cosmonauts are taking the torch for the Sochi Winter Olympics on its first space journey ...</p>
+                        <p>The UK fell silent in tribute to service personnel who have died during conflicts, as part of the annual Remembrance Sunday service. ...</p>
                     </div>
 
                     <div class="story">
